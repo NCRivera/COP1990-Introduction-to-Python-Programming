8b2ddadc5ce970dbd8761e3f533687a9544d2e77
NCRivera <41407150+NCRivera@users.noreply.github.com>
2018-08-29 19:22:37 -0400

    Add files via upload
    Second class day

---
+++ hello_user.py
@@ -0,0 +1,16 @@
+#input output example
+#Nicholas Rivera
+#08/29/18
+
+
+#Ask user for their first name
+#Store the first name
+first_name = input('Enter your first name: ')
+
+
+#Ask user for their last name
+#Store the last name
+last_name = input('Enter your last name: ')
+
+#Say hello to the user
+print('Hello,', first_name, last_name + '!')
---
+++ test_score.py
@@ -0,0 +1,19 @@
+#get three test scores, NUMBERS 0 - 100.
+
+#get score 1
+#store the score1
+score_1 = int(input('What is the first test score: '))
+
+#get score 2
+#store the score2
+score_2 = int(input('What is the second test score: '))
+
+#get score 3
+#store the score3
+score_3 = int(input('What is the third test score: '))
+
+#calculate average by adding the 3 scores together and divide by 3
+average = (score_1 + score_2 + score_3)/3
+
+#display average
+print(average)
