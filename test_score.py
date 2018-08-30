247c0a95de00e06cb0bf86850d04bdad1b3ee790
NCRivera <41407150+NCRivera@users.noreply.github.com>
2018-08-29 21:27:57 -0400

    Update test_score.py

--- test_score.py
+++ test_score.py
@@ -2,18 +2,18 @@
 
 #get score 1
 #store the score1
-score_1 = int(input('What is the first test score: '))
+score_1 = float(input('What is the first test score: '))
 
 #get score 2
 #store the score2
-score_2 = int(input('What is the second test score: '))
+score_2 = float(input('What is the second test score: '))
 
 #get score 3
 #store the score3
-score_3 = int(input('What is the third test score: '))
+score_3 = float(input('What is the third test score: '))
 
 #calculate average by adding the 3 scores together and divide by 3
-average = (score_1 + score_2 + score_3)/3
+average = (score_1 + score_2 + score_3)/3.0
 
 #display average
-print(average)
+print("The average score is: ", str(average))
