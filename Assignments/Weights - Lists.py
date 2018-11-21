dd2dc0f8d1a5ea52689835edd707ce17a924a18c
NCRivera <41407150+NCRivera@users.noreply.github.com>
2018-11-21 10:48:43 -0500

    Add files via upload
    Hardest Assignments I've had so far in Python. ZyBooks is very picky when you use there labs for assignments. Output must be exact. Spaces, new lines, etc.

---
+++ Assignments/Weights - Lists.py
@@ -0,0 +1,9 @@
+# FIXME (1): Prompt for four weights. Add all weights to a list. Output list.
+
+# FIXME (2): Output average of weights.
+
+# FIXME (3): Output max weight from list.
+
+# FIXME (4): Prompt the user for a list index and output that weight in pounds and kilograms.
+
+# FIXME (5): Sort the list and output it.
